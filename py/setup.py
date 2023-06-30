58576b07b996c8ce1653176e0be1974bf632f655
ariverolq <95715132+ariverolq@users.noreply.github.com>
2023-06-30 15:01:44 -0300

    Update setup.py

--- py/setup.py
+++ py/setup.py
@@ -15,5 +15,5 @@ setup(
    data_files=[('serverRoberta', ['serverRoberta/config.ini'])],
    py_modules=['serverRoberta.roberta_pb2', 'serverRoberta.roberta_pb2_grpc'],
    include_package_data=True,
-   install_requires=['fairseq==0.10.0', 'scipy==1.5.0', 'scikit-learn==0.23.1','pandas==1.3.4','pyparsing==3.0.6','torch==1.10.0','tqdm==4.62.3','wget==3.2','grpcio-tools==1.29.0','requests'],
+   install_requires=['fairseq==0.10.0', 'scipy==1.5.0', 'scikit-learn==0.23.1','numpy==1.23.5','pandas==1.3.4','pyparsing==3.0.6','torch==1.10.0','tqdm==4.62.3','wget==3.2','grpcio-tools==1.29.0','requests'],
 )
