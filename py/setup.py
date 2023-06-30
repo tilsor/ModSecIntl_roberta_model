9e3b848d65124d62a66aba5fae96e12209f940cc
ariverolq <95715132+ariverolq@users.noreply.github.com>
2023-06-30 15:33:08 -0300

    Update setup.py

--- py/setup.py
+++ py/setup.py
@@ -6,6 +6,8 @@ setup(
    name='server-roberta',
    version='0.4',
    description='To classify, BPE, ROBERTA and SVM are used',
+   long_description_content_type="text/markdown",
+   long_description=README,
    author='Tilsor',
    author_email='tilsor@tilsor.com.uy',
    setup_requires='setuptools',
